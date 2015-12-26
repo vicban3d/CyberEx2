df8e39fecc6f04950c06251eabd99b04420f310f
victor <vicban3d@gmail.com>
2015-12-26 21:11:19 +0200

    fix

--- part3.py
+++ part3.py
@@ -17,11 +17,11 @@ def handle_packet(pkt):
 	global response
 	scapy_packet = IP(pkt.get_payload())
 	
-	if IP in scapy_packet and TCP in scapy_packet: 
-		
+	if IP in scapy_packet and TCP in scapy_packet: 		
 		payload  = scapy_packet[TCP].payload		
 		header = str(payload).split(' ')
 
+		# Check for attempts to request a forbidden file
 		if len(header) != 0 and header[0] == 'GET':
 			path = header[1]
 			filename = path.split('/')[len(path.split('/')) - 1]
@@ -29,32 +29,32 @@ def handle_packet(pkt):
 			if extension in blacklist:
 				if not silent:					
 					print 'Detected request for', extension, 'file. ACTION: DROP'
-				pkt.accept()	
+				pkt.drop()	
 			else:
 				pkt.accept()		
 		else:
 			dst_port = scapy_packet['TCP'].dport
 			src_ip = scapy_packet['IP'].src
-			dst_ip = scapy_packet['IP'].dst			
+			dst_ip = scapy_packet['IP'].dst
 			
-			# *** DISCOVER LOCAL SUBNETS *** #
+			# discover all subnets
 			internal_nets = []
 			addresses = [get_if_addr(i) for i in get_if_list()]
 			for addr in addresses:
-				addr = addr.split('.')[0] + '.' + addr.split('.')[1] + '.' + addr.split('.')[2] + '.0'		
-				internal_nets.append(addr)
+				internal_nets.append(get_subnet_from_ip(addr))
 			
-			dst_net = dst_ip.split('.')[0] + '.' + dst_ip.split('.')[1] + '.' + dst_ip.split('.')[2] + '.0'
-			src_net = src_ip.split('.')[0] + '.' + src_ip.split('.')[1] + '.' + src_ip.split('.')[2] + '.0'
-
-			if dst_port == SSH_PORT and dst_net in internal_nets:
+			# Check for ssh session attempts
+			if dst_port == SSH_PORT and get_subnet_from_ip(dst_ip) in internal_nets:
 				if not silent:
-					print 'Detected remote shell execution attempt from', src_ip ,'. ACTION: DROP'
+					print 'Detected remote shell execution attempt from', str(src_ip) + '. ACTION: DROP'
 				pkt.drop()				
 			else:
 				pkt.accept()
 	else:
 		pkt.accept()
+
+def get_subnet_from_ip(ip):
+	return ip.split('.')[0] + '.' + ip.split('.')[1] + '.' + ip.split('.')[2] + '.0'
 
 
 if __name__ == '__main__':
