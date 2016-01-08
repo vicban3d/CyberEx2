d28ef2796dabcee9c8b9205174ffcf4c4a80eea9
victor <vicban3d@gmail.com>
2016-01-08 13:10:40 +0200

    done

--- part3.py
+++ part3.py
@@ -14,58 +14,53 @@ TCP_SYN = 0x02
 TCP_SYN_ACK = 0x12
 TCP_ACK = 0x10
 RST_FLAG = 0x04
-MAX_FRAGMENT_CACHE = 4
+MAX_FRAGMENT_CACHE = 10
 MAX_CONNECTION_BUFFER_SIZE = 64
 packet_buffer = {}
-fragment_number = 0
 internal_nets = []
-
 
 def handle_packet(pkt):
     global packet_buffer
-    global fragment_number
     global internal_nets
 
     current_packet = IP(pkt.get_payload())
     dst_port = current_packet['TCP'].dport
     src_port = current_packet['TCP'].sport
     src_ip = current_packet['IP'].src
-    dst_ip = current_packet['IP'].dst
-
-    # identifier for the current packet
-    routing_tuple = str(src_ip) + '-' + str(dst_ip) + '-' + str(
-        src_port) + '-' + str(dst_port)
-    routing_tuple_reverse = str(dst_ip) + '-' + str(src_ip) + '-' + str(
-        dst_port) + '-' + str(src_port)
+    dst_ip = current_packet['IP'].dst	    
 
     # check if new connection and initialize buffer
-    if routing_tuple not in packet_buffer.keys():
+    if not existing_connection(src_ip, dst_ip, src_port, dst_port):
         # handle possible buffer overflow exploit
         if len(packet_buffer) > MAX_CONNECTION_BUFFER_SIZE:
             print '[WARNING] Connection buffer overflow, dropping all traffic.'
             pkt.drop()
             return
         else:
-            packet_buffer[routing_tuple] = ''
-            packet_buffer[routing_tuple_reverse] = ''
+        	# initialize packet buffer.
+            routing_tuple = str(src_ip) + '-' + str(dst_ip) + '-' + str(src_port) + '-' + str(dst_port)
+            packet_buffer[routing_tuple] = {}
 
+    routing_tuple = get_connection_key(src_ip, dst_ip, src_port, dst_port)
     # detected TCP traffic
     if IP in current_packet and TCP in current_packet:
-        payload = current_packet['TCP'].payload
-        packet_buffer[routing_tuple] += str(payload)
-        if current_packet['IP'].flags == 1 and fragment_number \
-                <= MAX_FRAGMENT_CACHE:
-            fragment_number += 1
-        elif fragment_number > MAX_FRAGMENT_CACHE:
-            fragment_number = 0
+        payload = current_packet['TCP'].payload       
+        # if htere are more fragments and not enough collected yet, add to buffer. 
+        if current_packet['IP'].flags == 1 and len(packet_buffer[routing_tuple]) <= MAX_FRAGMENT_CACHE:
+            packet_buffer[routing_tuple][current_packet['TCP'].seq] = str(payload)
+            payload_so_far = assemble_payload_fragments(packet_buffer[routing_tuple])  
+        # if too many fragments colelcted, accept and reset buffer for current connection.
+        elif len(packet_buffer[routing_tuple]) > MAX_FRAGMENT_CACHE:
             packet_buffer.pop(routing_tuple)
-            packet_buffer.pop(routing_tuple_reverse)
             pkt.accept()
             return
-
+        else:
+        	# else there is only one fragment which should be inspected.
+        	payload_so_far = str(payload)
+              
         # split TCP payload into parts (in case of HTTP traffic we will get
         # [METHOD] [PATH] [VERSION] at the beginning)
-        header = packet_buffer[routing_tuple].split(' ')
+        header = payload_so_far.split(' ')
         # check for attempts to request a forbidden file
         if len(header) != 0 and header[0] == 'GET':
             path = header[1]
@@ -81,10 +76,7 @@ def handle_packet(pkt):
                     send_reset(current_packet)
                 print '[DROP] Request for a forbidden file extension (' \
                       + extension + ').'
-
                 packet_buffer.pop(routing_tuple)
-                packet_buffer.pop(routing_tuple_reverse)
-
                 pkt.drop()
             else:
                 pkt.accept()
@@ -105,14 +97,13 @@ def handle_packet(pkt):
                     print '[DROP] Attempt to establish SSH connection from', \
                         current_packet['IP'].src + '.'
                     packet_buffer.pop(routing_tuple)
-                    packet_buffer.pop(routing_tuple_reverse)
                     pkt.drop()
                 else:
                     pkt.accept()
             # if connection is not an SSH attempt, but an HTTP response
             # check if file extension in the body was spoofed (magic)
-            elif packet_buffer[routing_tuple][0:15] == 'HTTP/1.1 200 OK':
-                body = packet_buffer[routing_tuple].split('\r\n\r\n')[1]
+            elif payload_so_far[0:15] == 'HTTP/1.1 200 OK':
+                body = payload_so_far.split('\r\n\r\n')[1]
                 # convert body to HEX in order to compare the magic numbers
                 body = str("".join("{:02x}".format(ord(c)) for c in body))
                 # check if body starts with any forbidden magic number
@@ -124,27 +115,45 @@ def handle_packet(pkt):
                               file_extensions[
                                   magic_numbers.index(magic)] + ').'
                         packet_buffer.pop(routing_tuple)
-                        packet_buffer.pop(routing_tuple_reverse)
                         pkt.drop()
-                        return
-                if fragment_number == 0:
-                    packet_buffer.pop(routing_tuple)
-                    packet_buffer.pop(routing_tuple_reverse)
+                        return 
+                packet_buffer.pop(routing_tuple)
                 pkt.accept()
             else:
-                if fragment_number == 0:
-                    packet_buffer.pop(routing_tuple)
-                    packet_buffer.pop(routing_tuple_reverse)
+                packet_buffer.pop(routing_tuple)
                 pkt.accept()
     else:
         pkt.accept()
 
 
+# checks whether the current connection already exists in the packet buffer as a key.
+def existing_connection(src_ip, dst_ip, src_port, dst_port):
+	routing_tuple = str(src_ip) + '-' + str(dst_ip) + '-' + str(src_port) + '-' + str(dst_port)
+	routing_tuple_reverse = str(dst_ip) + '-' + str(src_ip) + '-' + str(dst_port) + '-' + str(src_port)  
+	return routing_tuple in packet_buffer.keys() or routing_tuple_reverse in packet_buffer.keys()
+
+# returns the conenction key while disregarding direction(c2s/s2c).
+def get_connection_key(src_ip, dst_ip, src_port, dst_port):
+	routing_tuple = str(src_ip) + '-' + str(dst_ip) + '-' + str(src_port) + '-' + str(dst_port)
+	if routing_tuple in packet_buffer.keys():
+		return routing_tuple
+	routing_tuple_reverse = str(dst_ip) + '-' + str(src_ip) + '-' + str(dst_port) + '-' + str(src_port)
+	if routing_tuple_reverse in packet_buffer.keys():
+		return routing_tuple_reverse
+
+# returns the assembled packet from fragments received so far.
+def assemble_payload_fragments(fragments):
+	res = ''
+	for seq in fragments.sort():
+		res += fragments[seq]
+	return res
+
+# infers a subnet adddress from IP
 def get_subnet_from_ip(ip):
     return ip.split('.')[0] + '.' + ip.split('.')[1] + '.' + ip.split('.')[
         2] + '.0'
 
-
+# returns a reset to the sender.
 def send_reset(packet):
     dst_port = packet['TCP'].dport
     src_port = packet['TCP'].sport
@@ -161,31 +170,18 @@ def send_reset(packet):
          verbose=False)
 
 
-if __name__ == '__main__':
-    # global internal_nets
-    # process commandline arguments
-    if len(sys.argv) == 1:
-        silent = False
-    elif len(sys.argv) == 2:
-        if sys.argv[1] == '-s':
-            silent = True
-        else:
-            print 'Usage: part3.py [-s]\n\t[-s] - Silent Mode: Client will' \
-                  ' not be notified of blocked traffic.'
-            exit()
-    else:
-        print 'Usage: part3.py [-s]\n\t[-s] - Silent Mode: Client will' \
-              ' not be notified of blocked traffic.'
-        exit()
-
+if __name__ == '__main__':  
     # place all packets marked for forwarding in queue
     os.system('iptables -A FORWARD -j NFQUEUE --queue-num 1')
 
     # read configuration file
     with open(conf_filename) as f:
         config = map(str.strip, f.readlines())
-    magic_numbers = filter(lambda mnum: mnum != '',
-                           [cur.split('-')[1] for cur in config])
+    # read silent mode config
+    silent = True if config[0].split('=')[1] == '1' else False
+    # skip to files
+    config = config[2:]
+    magic_numbers = filter(lambda mnum: mnum != '', [cur.split('-')[1] for cur in config])
     file_extensions = [x.split('-')[0] for x in config]
 
     print 'Started Packet Filter in ' + (
